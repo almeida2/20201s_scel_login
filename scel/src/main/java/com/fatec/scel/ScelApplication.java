5cd5d15901d561c5479c96e1f2c6ce17056e1d86
55119 <55119@192.168.0.6>
2020-03-20 10:14:44 -0300

    Versao Inicial - login pelo github

---
+++ scel/src/main/java/com/fatec/scel/ScelApplication.java
@@ -0,0 +1,16 @@
+package com.fatec.scel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//https://medium.com/@nataniel.paiva/oauth2-com-spring-boot-2-e-spring-5-e7bfb7c58d4a
+//https://spring.io/guides/tutorials/spring-boot-oauth2/
+@SpringBootApplication
+public class ScelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScelApplication.class, args);
+	}
+
+}
