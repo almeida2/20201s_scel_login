e107554bbe7cbeccb8bacf02baed4565c303324d
55119 <55119@192.168.0.6>
2020-03-21 14:37:47 -0300

    end point user

--- scel/src/main/java/com/fatec/scel/ScelApplication.java
+++ scel/src/main/java/com/fatec/scel/ScelApplication.java
@@ -1,14 +1,26 @@
 package com.fatec.scel;
 
+import java.util.Collections;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 
 //https://medium.com/@nataniel.paiva/oauth2-com-spring-boot-2-e-spring-5-e7bfb7c58d4a
 //https://spring.io/guides/tutorials/spring-boot-oauth2/
-@SpringBootApplication
-public class ScelApplication {
 
+@SpringBootApplication
+@RestController
+public class ScelApplication {
+	 @GetMapping("/user")
+	    public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
+	        return Collections.singletonMap("name", principal.getAttribute("name"));
+	    }
 	public static void main(String[] args) {
 		SpringApplication.run(ScelApplication.class, args);
 	}
